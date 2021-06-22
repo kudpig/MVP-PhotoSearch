487ed0cad569c99676717d7482b6bb982eb5d7ee
kudpig <shin.kudo.aa@gmail.com>
2021-06-22 13:51:47 +0900

    [setting]コードでアプリ起動

--- MVP-PhotoSearch/SearchPhotoViewController.swift
+++ MVP-PhotoSearch/SearchPhotoViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchPhotoViewController.swift
 //  MVP-PhotoSearch
 //
 //  Created by Shinichiro Kudo on 2021/06/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchPhotoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
