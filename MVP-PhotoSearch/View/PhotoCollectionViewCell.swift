3fe0dc6db5ebc7363a29d9564bd2d89ddd0491e1
kudpig <shin.kudo.aa@gmail.com>
2021-06-29 18:33:54 +0900

    Update PhotoCollectionViewCell.swift

--- MVP-PhotoSearch/View/PhotoCollectionViewCell.swift
+++ MVP-PhotoSearch/View/PhotoCollectionViewCell.swift
@@ -16,7 +16,6 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     func configure(model: PhotoModel) {
